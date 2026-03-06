2d697616697ab81240fe75ca6c46cc8f404e21f3
DevopsAtharva21 <devopsatharva29@gmail.com>
2026-03-06 19:53:37 +0000

    added hello.sh

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+echo "Hello dosto"
+read -p "Rancho ka word" word
+
+echo "Silencer: Desh bhar me $word kiye"
+
+echo "Silencer: College me $word kiye"
+
+echo "Rancho: Ratta maregea toh aisa hi hoga"
